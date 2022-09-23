0095bb7b3981ff8518003888cd89095c4107074b
kbm <dev.kbmapp@gmail.com>
2022-09-23 13:38:30 +0700

    edit Latihan09

--- Praktikum-01-Runtunan/src/Latihan09.java
+++ Praktikum-01-Runtunan/src/Latihan09.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Students {
     int npm;
     String fullName, className;
@@ -28,5 +30,26 @@ class Students {
 public class Latihan09 {
     public static void main(String[] args) {
         Students myBio = new Students();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan Nama : ");
+        String name = input.nextLine();
+        System.out.print("Masukkan Kelas : ");
+        String kelas = input.nextLine();
+        System.out.print("Masukkan NPM : ");
+        int npm = input.nextInt();
+        System.out.print("Masukkan IPK : ");
+        float gpa = input.nextFloat();
+
+        System.out.println("==============================");
+
+        System.out.println("Nama : " + myBio.getFullName(name));
+        System.out.println("Kelas : " + myBio.getclassName(kelas));
+        System.out.println("NPM : " + myBio.getNPM(npm));
+        System.out.println("IPK : " + myBio.getGPA(gpa));
+
+        input.close();
+
     }
 }
